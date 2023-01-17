a91a1002ffd89c5c572f9ef84976dcc100743712
Vincent Hamp <vincent.hamp@higaski.at>
2023-01-17 18:54:07 +0100

    Link tests component with gtest and gmock

--- tests/app_main.cpp
+++ tests/app_main.cpp
@@ -1,4 +1,22 @@
-#include <esp_system.h>
-#include "sum.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-extern "C" void app_main() { esp_restart(); }
+namespace {
+
+int argc;
+char** argv;
+
+// This is a dirty workaround to copy argc/argv before it disappears into the
+// depths of ESP-IDF.
+[[gnu::constructor]] void copy_argc_argv(int argc, char** argv) {
+  ::argc = argc;
+  ::argv = argv;
+}
+
+}  // namespace
+
+extern "C" void app_main() {
+  testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleMock(&argc, argv);
+  exit(RUN_ALL_TESTS());
+}
